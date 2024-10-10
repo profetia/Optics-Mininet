9a47c5a8f451b39118d4c3929a605d5062ae07df
lyang <lyang@mpi-info.mpg.de>
2024-10-10 15:31:38 +0200

    Fix wrong tor_id

--- src/runtime/rpc.py
+++ src/runtime/rpc.py
@@ -7,18 +7,30 @@ from proto.rpc_pb2 import PauseFlowRequest, ResumeFlowRequest
 from proto.rpc_pb2_grpc import RpcStub
 
 import stub.collect as collect
-import stub.util as util
 
 import time
 
 
+Uranus = "switch6-uranus"
+Neptune = "switch6-neptune"
+
+
+Hosts = {
+    Uranus: "10.0.13.23",
+    Neptune: "10.0.13.24",
+}
+
+
 class Client:
 
-    def __init__(self, url: str):
+    def __init__(self, host: str):
+        self.host = host
+        self.tor_range = range(4) if host == Uranus else range(4, 8)
+
         self.channels: list[grpc.aio.Channel] = []
         self.stubs: list[RpcStub] = []
         for i in range(4):
-            channel = grpc.aio.insecure_channel(f"{url}:{4000 + i}")
+            channel = grpc.aio.insecure_channel(f"{Hosts[host]}:{4000 + i}")
             self.channels.append(channel)
             self.stubs.append(RpcStub(channel))
 
@@ -30,15 +42,14 @@ class Client:
         set_afc_entries = collect.pause_flow_impl(tor_id, schedule)
         request = PauseFlowRequest(set_afc_table_entries=set_afc_entries)
 
-        await self.stubs[tor_id].PauseFlow(request)
+        await self.stubs[tor_id % 4].PauseFlow(request)
 
     async def pause_flow(self, schedule: np.ndarray) -> None:
         await asyncio.gather(
-            *[self.__pause_flow_impl(tor_id, schedule) for tor_id in range(4)]
+            *[self.__pause_flow_impl(tor_id, schedule) for tor_id in self.tor_range]
         )
 
     async def __resume_flow_impl(self, tor_id: int, schedule: np.ndarray) -> None:
-
         hoho_lookup_send_slice_entries, slice_to_direct_tor_ip_entries = (
             collect.resume_flow_impl(tor_id, schedule)
         )
@@ -47,29 +58,28 @@ class Client:
             slice_to_direct_tor_ip_table_entries=slice_to_direct_tor_ip_entries,
         )
 
-        await self.stubs[tor_id].ResumeFlow(request)
+        await self.stubs[tor_id % 4].ResumeFlow(request)
 
     async def resume_flow(self, schedule: np.ndarray) -> None:
         await asyncio.gather(
-            *[self.__resume_flow_impl(tor_id, schedule) for tor_id in range(4)]
+            *[self.__resume_flow_impl(tor_id, schedule) for tor_id in self.tor_range]
         )
 
 
-Hosts = {
-    "switch6-uranus": "10.0.13.23",
-    "switch6-neptune": "10.0.13.24",
-}
-
-
 async def __main():
-    cli = Client(url=Hosts["switch6-neptune"])
+    schedule = np.loadtxt(
+        "runtime/8tors_1ports_test.txt",
+        dtype=int,
+    )
+
+    cli = Client(host=Neptune)
 
     start = time.time_ns()
 
     await asyncio.gather()
 
-    await cli.pause_flow(util.default_schedule)
-    await cli.resume_flow(util.default_schedule)
+    await cli.pause_flow(schedule)
+    await cli.resume_flow(schedule)
 
     end = time.time_ns()
     print(f"Time elapsed: {(end - start) / 1_000_000} ms")
